0ed3d78de42aa150c4eca6d8f6d2540c3d246960
Tom Raithel <t.raithel@googlemail.com>
2017-03-08 21:01:38 +0100

    Add babel polyfill and tweak some styles for firefox

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -1,22 +1,21 @@
 import React from 'react';
+import classNames from 'classnames';
 import Branding from './Branding';
 import { getItemPageNames } from '../../common/config';
 
 export default function Footer({ items, pageName }) {
-  if (getItemPageNames(items).includes(pageName)) {
-    return null;
-  }
-
   return (
-    <Branding
-      logoContent={<img src="/assets/logo.svg" width="150px" height="60px" />}
-    >
-      <span className="footnote">
-        AOE is a leading provider of Enterprise Open Source web solutions.
-        Using current agile development methods, more than 250+ developers
-        and consultants in 8 global locations develop customized Open Source
-        solutions for global companies and corporations.
-      </span>
-    </Branding>
+    <div className={classNames('footer', {'is-hidden': getItemPageNames(items).includes(pageName)})}>
+      <Branding
+        logoContent={<img src="/assets/logo.svg" width="150px" height="60px" />}
+      >
+        <span className="footnote">
+          AOE is a leading provider of Enterprise Open Source web solutions.
+          Using current agile development methods, more than 250+ developers
+          and consultants in 8 global locations develop customized Open Source
+          solutions for global companies and corporations.
+        </span>
+      </Branding>
+    </div>
   );
 }
