64705b5df857588bbbe80cf2f47bd3869eb5d208
Tom Raithel <t.raithel@googlemail.com>
2017-03-08 22:47:14 +0100

    CSS adjustments for search

---
+++ js/components/Icon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import actions from '../actions';
+
+// import question from '../../assets/icons/question.svg';
+
+function Icon({ name, ...props }) {
+  return (
+    <span/>
+  );
+}
--- js/components/Search.js
+++ js/components/Search.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
 
-export default function Search({ value, onChange }) {
+export default function Search({ value, onChange, onSubmit = () => {} }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit();
+  };
+
   return (
-    <div className="search">
-      <input value={value} type="text" onChange={(e) => { onChange(e.target.value); }} className="search__field" placeholder="Search" />
-      <span className="icon icon--search search__icon" />
-    </div>
+    <form className="search" onSubmit={handleSubmit}>
+      <input
+        value={value}
+        type="text"
+        onChange={(e) => { onChange(e.target.value); }}
+        className="search__field"
+        placeholder="What are you looking for?"
+      />
+      <span className="search__button">
+        <button type="submit" className="button">
+          <span className="icon icon--search button__icon" />
+          Search
+        </button>
+      </span>
+    </form>
   );
 }
