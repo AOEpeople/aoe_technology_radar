7a5b88680c19d1495b6a08109e4c8d323a95356c
Tom Raithel <t.raithel@googlemail.com>
2017-03-21 21:48:17 +0100

    Fix page title if page type does not change

--- js/components/SetTitle.js
+++ js/components/SetTitle.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+
+const callSetTitle = (props) => {
+  if (typeof props.onSetTitle === 'function' && props.title) {
+    props.onSetTitle(props.title);
+  }
+};
+
 class SetTitle extends React.Component {
   constructor(props) {
     super(props);
-    if (typeof props.onSetTitle === 'function' && props.title) {
-      props.onSetTitle(props.title);
+    callSetTitle(props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.title !== this.props.title) {
+      callSetTitle(nextProps);
     }
   }
 
