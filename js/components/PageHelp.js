f77e1c6d96f6860a977b67d5e23f1ae29f928aff
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2017-03-31 19:17:37 +0200

    Add Help page content

--- js/components/PageHelp.js
+++ js/components/PageHelp.js
@@ -9,7 +9,37 @@ export default function PageHelp({ leaving, onLeave, ...props }) {
       <SetTitle {...props} title="How to use AOE Technology Radar" />
       <HeroHeadline>How to use AOE Technology Radar</HeroHeadline>
       <div className="fullpage-content">
-        <p>See also <a href="aoe-toolbox.html">AOE Toolbox</a></p>
+        <h3>Introduction</h3>
+        <p>Technology is moving fast and new technologies and innovations appear continuously.</p>
+        <p>It's essential for a development and technology company such as AOE to constantly improve and keep track with the latest useful innovations.
+        It is important to openly look for innovations and new technologies and to question established technologies and methods every now and then.</p>
+        <p>But, it is also important to wisely choose which technologies to use in our daily work and in the different projects we are carrying out. As we all know: There is no silver bullet.</p>
+        <h3>What is the AOE Tech Radar</h3>
+        <p>The Tech Radar is an overview of different technologies - from languages, frameworks, tools and patterns to platforms&nbsp;- that we consider "new or mentionable".
+          The radar therefore doesn't provide an overview of all&nbsp;established technologies&nbsp;- but it focuses on items that have recently gained in importance or changed.
+        </p>
+        <h3 >How it is created</h3><p>The items in the technology radar are raised by the different teams and therefore a lot of the items are related to the work and challenges the teams face in the different projects. In fact, we don't include anything on the radar, which we haven't already tried ourselves at least once.</p>
+        <p>There have been a lot of valuable discussions in different expert groups about the classification and details of each of technologies and innovations. And the result of all this can be found in the latest technology radar.</p>
+        <h3 >How should it be used</h3>
+        <p>The radar acts as an overview of technologies that we think everyone in the teams should currently know about.</p>
+        <p>Its goal is to act as a guide and inspiration for the daily work in the teams. Its purpose is also to provide helpful information and a bird's-eye perspective&nbsp;- so that decisions can be taken with a much deeper understanding of the subject matter. This results in more-informed and better-aligned decisions.</p>
+        <p>We also hope that developers outside of AOE find the informations in our technologie overview inspirational.</p>
+        <p>We group or categorize the items in 4 quadrants&nbsp;- (sometimes, when it's not 100% clear where a item belongs, we choose the best fit).</p>
+        <p>The quadrants are:</p>
+        <ul>
+          <li><strong>Languages and Frameworks:</strong> We've placed development languages (such as Scala or Golang) here, as well as more low-level development frameworks (such as Play or Symfony), which are useful for implementing custom software of all kinds. </li>
+          <li><strong>Tools:</strong> Here we put different software tools - from small helpers to bigger software projects</li>
+          <li><strong>Methods and Patterns:</strong> Patterns are so important, and a lot of them are valid for a long time (compared to some tools or frameworks). So, this is the category where we put information on methods and patterns concerning development, continuous x, testing, organization, architecture, etc.</li>
+          <li><strong>Platforms and Services</strong> (including AOE internal Services): Here we include infrastructure platforms and services. We also use this category to communicate news about AOE services that we want all AOE teams to be aware of.</li>
+        </ul>
+        <p>Each of the items is classified in one of these rings:</p>
+        <ul>
+          <li><strong>Adopt:</strong> We can clearly recommend this technology. We have used it for longer period of time in many teams and it has proven to be stable and useful.</li>
+          <li><strong>Trial:</strong> We have used it with success and recommend to have a closer look at the technology in this ring. The goal of items here is to look at them more closely, with the goal to bring them to the adopt level.</li>
+          <li><strong>Assess:</strong> We have tried it out and we find it promising. We recommend having a look at these items when you face a specific need for the technology in your project.</li>
+          <li><strong>Hold:</strong> This category is a bit special. Unlike the others, we recommend to stop doing or using something. We move things here if we think we shouldn't do or use them (anymore).</li>
+        </ul>
+        <p>We also maintain a short list of useful tools - that are not worth putting on the radar - but that can belong in everyone's "toolbox". We call the list of these technologies the AOE Toolbox and you can find information about them here: <a href="aoe-toolbox.html">AOE Toolbox</a></p>
       </div>
     </Fadeable>
   );
--- js/components/PageToolbox.js
+++ js/components/PageToolbox.js
@@ -10,31 +10,31 @@ export default function PageToolbox({ leaving, onLeave, ...props }) {
       <HeroHeadline>Small AOE Toolbox</HeroHeadline>
       <div className="fullpage-content">
         <h3>Useful Tools</h3>
-        <p>
-          <ul>
-            <li>Fiddler - free web debugging proxy ( http://www.telerik.com/fiddler )</li>
-            <li>SoapUI - Webservice Test Tool (https://www.soapui.org/ )</li>
-            <li>Postman - API Test Tool ( https://www.getpostman.com/ )</li>
-            <li> Modelio  - Simple free UML Modelling tool ( https://www.modelio.org/ )</li>
-          </ul>
-        </p>
+
+        <ul>
+          <li>Fiddler - free web debugging proxy ( http://www.telerik.com/fiddler )</li>
+          <li>SoapUI - Webservice Test Tool (https://www.soapui.org/ )</li>
+          <li>Postman - API Test Tool ( https://www.getpostman.com/ )</li>
+          <li> Modelio  - Simple free UML Modelling tool ( https://www.modelio.org/ )</li>
+        </ul>
+        
         <h3>Useful Tools (commercial)</h3>
-        <p>
-          <ul>
-            <li> Paw Rest Client</li>
-          </ul>
-        </p>
+
+        <ul>
+          <li> Paw Rest Client</li>
+        </ul>
+
 
         <h3>Estabilshed Technologies</h3>
         <p>
           Not mentionable but adopted for a while now:
-          <ul>
-            <li>Jenkins</li>
-            <li>Redis</li>
-            <li>Varnish</li>
-            <li>Symfony2</li>
-          </ul>
         </p>
+        <ul>
+          <li>Jenkins</li>
+          <li>Redis</li>
+          <li>Varnish</li>
+          <li>Symfony2</li>
+        </ul>
       </div>
     </Fadeable>
   );
