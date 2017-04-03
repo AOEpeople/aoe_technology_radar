37d9233e67e27514ae587c772d903867db2ba715
Mathias Schopmans <mathias.schopmans@aoe.com>
2017-04-03 16:57:15 +0200

    Add modifier to branding component

--- js/components/Branding.js
+++ js/components/Branding.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classNames from 'classnames';
 
-export default function Branding({ logoContent, children }) {
+export default function Branding({ logoContent, modifier, children }) {
   return (
-    <div className="branding">
+    <div className={classNames('branding', { [`branding--${modifier}`]: modifier })}>
       <div className="branding__logo">
         {logoContent}
       </div>
