4ca88413b14dc4e0aefb28928240a2656e5c30e5
Kofdrafo <rainer.zehnle@gmai.com>
2017-12-06 14:34:37 +0100

    Changed text why ww created the techradar

--- js/components/PageHelp.js
+++ js/components/PageHelp.js
@@ -6,26 +6,32 @@ import SetTitle from './SetTitle';
 export default function PageHelp({ leaving, onLeave, ...props }) {
   return (
     <Fadeable leaving={leaving} onLeave={onLeave}>
-      <SetTitle {...props} title="How to use Haufe Technology Radar" />
-      <HeroHeadline>How to use Haufe Technology Radar</HeroHeadline>
+      <SetTitle {...props} title="About the Haufe Technology Radar" />
+      <HeroHeadline>About the Haufe Technology Radar</HeroHeadline>
       <div className="fullpage-content">
         <h3>Introduction</h3>
         <p>Technology gets more and more important for each company and especially for software vendors.<br/>
         The Haufe Group is considered one of the most innovative media and software vendors in Germany. Its solutions use state-of-the-art technology and are very user-friendly and practice-oriented.<br/>
         It's essential for our business to constantly keep track on new technologies and useful innovations.
         Not each technology fits to our requirements and strategy. It's important to choose them wisely.</p>
-        <h3>What is the Haufe Tech Radar</h3>
-        <p>The Tech Radar is an overview of different technologies and innovations that are important for our business.
-          The radar doesn't provide an overview of all technologies - but it focuses on technologies and innovations that have or might have business impact on our strategy.
-          It's goal is to reflect our maturity state and experience with these technologies.
-        </p>
-        <h3 >How it is created</h3>
+        <h3>What is the Haufe Tech Radar?</h3>
+        <p>The Tech Radar is an overview of technologies and innovations that are important for our business.
+          It displays new and upcoming technologies relevant for the Haufe Group.
+          The radar doesn't provide an overview of all technologies - but it focuses on key-technologies and innovations that have or might have business impact on our strategy.
+          It's goal is to reflect our maturity state and experience with these technologies. It also an instrument to measure our progress over time.</p>
+        <p>
+          Aim and mission is to help Haufe Group employees discovering all the new tech, that is used to realize customer-centric solutions and services. 
+          We want to foster the knowledge about technologies and relevant projects giving like-minded people the possibility to connect easily and share their passion for each topic. 
+          In this way we want to unlock synergies and knowledge transfer regrading ideas, success stories and pitfalls.
+          As technologies show up quickly in the market, the Radar is updated constantly and therefore is always kind of work in progress.
+        </p>    
+        <h3 >How it is created and maintained</h3>
         <p>The items in the technology radar are maintained by the innovation team.<br/>
         It's always a hard discussion about the classification and details of each technology and innovation. Please contribute to the discussion. It is the tech radar of our company and not from a single group.</p>
         <h3 >How should it be used</h3>
         <p>The radar acts as an overview of technologies that we think everyone in the company should currently know about.
         Its purpose is to provide helpful information and a bird's-eye perspective. It informs about our initiatives in these areas and hopefully helps to get in contact with other teams for experience exchange and collaboration.
-        It also helps to have an overview over our own maturity state with diferent technologies.</p>
+        It also helps to have an overview over our own maturity state with different technologies.</p>
         <p>We group or categorize the items in 4 quadrants - (sometimes, when it's not 100% clear where a item belongs, we choose the best fit).</p>
         <p>The quadrants are:</p>
         <ul>
@@ -42,8 +48,6 @@ export default function PageHelp({ leaving, onLeave, ...props }) {
         </ul>
       <p>Contributions and source code of the radar are on github: <a href="https://github.com/Haufe-Lexware/aoe_technology_radar" target="_blank">Haufe Tech Radar on Github</a></p>
       <p>The solution is forked from the AOE Tech Radar <a href="" target="_blank">AOE Tech Radar on Github</a>. We like to thank that guys for their great work!</p>
-
-
       </div>
     </Fadeable>
   );
