445c53cab319429e80c3cd9fa851d819867cfae4
Tom Clift <tom.clift@papercut.com>
2020-05-25 13:00:14 +1000

    refactor: remove duplicate quadrant name definition

--- tasks/template.js
+++ tasks/template.js
@@ -1,5 +1,6 @@
 import pug from 'pug';
 import moment from 'moment';
+import { translate } from '../common/config';
 import { relativePath } from '../common/file';
 import {
   groupByQuadrants,
@@ -10,22 +11,13 @@ import {
 const templateFolder = 'templates';
 
 export const vars = (vars) => ({
-  translate: (text) => {
-    const mappings = {
-      'languages-and-frameworks': 'Languages & Frameworks',
-      'methods-and-patterns': 'Methods & Patterns',
-      'platforms-and-aoe-services': 'Platforms and AOE Services',
-      'tools': 'Tools',
-    };
-
-    return mappings[text.trim()] || '-';
-  },
+  translate: translate,
   formatRelease: (release) => moment(release, 'YYYY-MM-DD').format('MMM YYYY'),
   groupByQuadrants,
   groupByFirstLetter,
   groupByRing,
   ...vars,
-})
+});
 
 export const item = pug.compileFile(relativePath(templateFolder, 'item-page.pug'));
 
