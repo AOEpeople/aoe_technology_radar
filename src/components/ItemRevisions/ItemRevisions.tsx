5c507c84e3612bef525a27277a1ac17183f056a1
Danny Koppenhagen <mail@d-koppenhagen.de>
2021-10-26 20:16:11 +0200

    feat(RevisionsText): make RevisionsText prefix configurable

--- src/components/ItemRevisions/ItemRevisions.tsx
+++ src/components/ItemRevisions/ItemRevisions.tsx
@@ -3,15 +3,17 @@ import HeadlineGroup from "../HeadlineGroup/HeadlineGroup";
 import ItemRevision from "../ItemRevision/ItemRevision";
 import { Revision } from "../../model";
 import "./item-revisions.scss";
+import { useMessages } from "../../context/MessagesContext";
 export default function ItemRevisions({
   revisions,
 }: {
   revisions: Revision[];
 }) {
+  const { revisionsText } = useMessages();
   return (
     <div className="item-revisions">
       <HeadlineGroup secondary>
-        <h4 className="headline headline--dark">Revisions:</h4>
+        <h4 className="headline headline--dark">{revisionsText ?? 'Revisions:'}</h4>
       </HeadlineGroup>
 
       {revisions.map((revision) => (
