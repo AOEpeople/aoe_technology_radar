92da3c1085fcfe8a2e51801901a20b1dfec50afe
Danny Koppenhagen <mail@d-koppenhagen.de>
2021-11-10 19:59:56 +0100

    feat: anitize descriptions on help page
    allow to customize description content by sanitizing it's content
    
    closes #102

--- src/components/PageHelp/PageHelp.tsx
+++ src/components/PageHelp/PageHelp.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import sanitizeHtml from 'sanitize-html';
 import HeroHeadline from "../HeroHeadline/HeroHeadline";
 import Fadeable from "../Fadeable/Fadeable";
 import SetTitle from "../SetTitle";
@@ -9,6 +10,13 @@ interface Props {
   leaving: boolean;
   onLeave: () => void;
 }
+
+const sanitize = (dirty: string, options: sanitizeHtml.IOptions = {}) => ({
+  __html: sanitizeHtml(
+    dirty,
+    options
+  )
+});
 
 const PageHelp: React.FC<Props> = ({ leaving, onLeave }) => {
   const { pageHelp } = useMessages();
@@ -21,19 +29,29 @@ const PageHelp: React.FC<Props> = ({ leaving, onLeave }) => {
         <HeroHeadline>How to use the {radarName}</HeroHeadline>
         <div className="fullpage-content">
           {paragraphs.map(({ headline, values }) => (
-            <React.Fragment key={headline}>
-              <h3>{headline}</h3>
-              {values.map((element, index) => (
-                <p key={index}>{element}</p>
-              ))}
-            </React.Fragment>
+              <React.Fragment key={headline}>
+                <h3>{headline}</h3>
+                {values.map((element, index) => {
+                  const content = sanitizeHtml(element, {
+                    allowedTags: ['b', 'i', 'em', 'strong', 'a', 'ul', 'ol', 'li'],
+                    allowedAttributes: {
+                      'a': ['href', 'target']
+                    },
+                  });
+                  console.log(content);
+                  return (
+                    <p key={index} dangerouslySetInnerHTML={sanitize(element)}></p>
+                  )
+                })
+              }
+              </React.Fragment>
           ))}
 
           <p>The quadrants are:</p>
           <ul>
             {quadrants.map(({ name, description }) => (
               <li key={name}>
-                <strong>{name}:</strong> {description}
+                <strong>{name}:</strong> <span dangerouslySetInnerHTML={sanitize(description)}></span>
               </li>
             ))}
           </ul>
@@ -42,7 +60,7 @@ const PageHelp: React.FC<Props> = ({ leaving, onLeave }) => {
           <ul>
             {rings.map(({ name, description }) => (
               <li key={name}>
-                <strong>{name}:</strong> {description}
+                <strong>{name}:</strong> <span dangerouslySetInnerHTML={sanitize(description)}></span>
               </li>
             ))}
           </ul>
