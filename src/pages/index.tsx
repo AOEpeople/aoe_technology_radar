4b5a7abbc9c6a885ef37fd9e9eafc1233dd519cc
Mathias Schopmans <mathias.schopmans@aoe.com>
2024-02-09 09:08:17 +0100

    chore: add .prettierrc and commit fixed files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import { Inter } from "next/font/google";
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
