8b44da35abd87de9be851e15b864515ed39ea3a5
Mathias Schopmans <mathias.schopmans@aoe.com>
2024-02-12 16:16:27 +0100

    chore: add icon components via svgr/cli

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 
 import styles from "./Navigation.module.css";
 
+import IconFilter from "@/components/Icons/Filter";
+import IconOverview from "@/components/Icons/Overview";
+import IconQuestion from "@/components/Icons/Question";
+import IconSearch from "@/components/Icons/Search";
 import { getAppName } from "@/lib/config";
 
 export function Navigation() {
@@ -10,14 +14,24 @@ export function Navigation() {
       <ul className={styles.list}>
         <li className={styles.item}>
           <Link href="/help-and-about-tech-radar">
+            <IconQuestion className={styles.icon} />
             How to use {getAppName()}?
           </Link>
         </li>
-        <li className={styles.item}>Filter</li>
         <li className={styles.item}>
-          <Link href="/overview">Technologies Overview</Link>
+          <IconFilter className={styles.icon} />
+          Filter
         </li>
-        <li className={styles.item}>Search</li>
+        <li className={styles.item}>
+          <Link href="/overview">
+            <IconOverview className={styles.icon} />
+            Technologies Overview
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <IconSearch className={styles.icon} />
+          Search
+        </li>
       </ul>
     </nav>
   );
