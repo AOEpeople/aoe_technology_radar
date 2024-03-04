ab005e03b310b3f1b3101a237cc83c08a900a97b
Mathias Schopmans <mathias.schopmans@aoe.com>
2024-03-04 15:36:57 +0100

    feat: cleanup

--- src/date.ts
+++
@@ -1,9 +0,0 @@
-import moment from "moment";
-
-const isoDateToMoment = (isoDate: moment.MomentInput) =>
-  moment(isoDate, "YYYY-MM-DD");
-
-export const formatRelease = (
-  isoDate: moment.MomentInput,
-  format: string = "MMMM YYYY"
-) => isoDateToMoment(isoDate).format(format);
