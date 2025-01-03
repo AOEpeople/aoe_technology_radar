9c0ff9405053e7b8ce13d32b6f6e801512a21f8c
Johannes Marx <marxjo@mediamarktsaturn.com>
2025-01-03 15:43:58 +0100

    chore: update dependencies

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from "next";
 
 import { getAbsoluteUrl, getItems, getQuadrants } from "@/lib/data";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const quadrants = getQuadrants().map((quadrant) => ({
     url: getAbsoluteUrl(`/${quadrant.id}/`),
